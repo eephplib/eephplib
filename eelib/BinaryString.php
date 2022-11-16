455eafa39b8127743486017f28e8f2d1b785b607
Travis van der Font <travis.font@gmail.com>
2022-11-16 17:36:19 +0100

    Add:  - BinaryString/find_first_occurrence  - BinaryString/replace_substrings
    Update:
     - BinaryString/regular_expression_split - throw exception

--- eelib/BinaryString.php
+++ eelib/BinaryString.php
@@ -2,6 +2,8 @@
 
 namespace eelib
 {
+    use RuntimeException;
+
     require_once 'Extended/BinaryString.php';
 
     /**
@@ -21,11 +23,68 @@ namespace eelib
          * @param   string      $string
          * @param   int         $limit
          *
-         * @return  string[]    The result as an array.
+         * @return  string[]   The result as an array.
          */
-        public static function regular_expression_split(string $pattern , string $string, int $limit = -1)
+        public static function regular_expression_split(
+            string  $pattern,
+            string  $string,
+            int     $limit = -1
+        ) : string
         {
             return \mb_split($pattern, $string, $limit);
+        }
+
+        /**
+         * Find the first occurrence of a string.
+         *
+         * @link    https://www.php.net/manual/en/function.strstr.php
+         *
+         * @param   string  $haystack       The input string.
+         * @param   string  $needle         The needle should either be explicitly cast to string,
+         *                                  or an explicit call to chr() should be performed.
+         * @param   bool    $before_needle  If true, strstr() returns the part of the haystack before
+         *                                  the first occurrence of the needle (excluding the needle).
+         *
+         *
+         * @return  string                  Returns part of haystack string starting from and
+         *                                  including the first occurrence  of needle to the end of haystack.
+ *
+         * @throws RuntimeException
+         */
+        public static function find_first_occurrence(
+            string  $haystack,
+            string  $needle,
+            bool    $before_needle = false
+        ) : string
+        {
+            $strstr = strstr($haystack, $needle, $before_needle);
+
+            if ($strstr === false) {
+                throw new RuntimeException('needle is not found');
+            }
+
+            return $strstr;
+        }
+
+        /**
+         * Translate characters or replace substrings.
+         *
+         * @link    https://www.php.net/manual/en/function.strtr.php
+         *
+         * @param   string  $string The string being translated.
+         * @param   string  $from   The string being translated to to.
+         * @param   string  $to     The string replacing from.
+         *
+         * @return  string          Returns the translated string.
+         */
+        public static function replace_substrings(
+            string $string,
+            string $from,
+            string $to
+        ) : string
+        {
+
+            return strtr($string, $from, $to);
         }
     }
 }
