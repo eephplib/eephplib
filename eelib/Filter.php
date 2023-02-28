1239bf479a411f750de1eeb3bdc3c6e5e9d24b3e
Travis van der Font <travis.font@gmail.com>
2023-02-28 23:35:14 +0100

    Added constant values to Filter class

--- eelib/Filter.php
+++ eelib/Filter.php
@@ -11,17 +11,21 @@ namespace eelib
 
     class Filter
     {
+        const VALIDATE_EMAI = \FILTER_VALIDATE_EMAIL; // Validating email addresses
+        const VALIDATE_IP   = \FILTER_VALIDATE_IP; // Validating IP addresses
+        const VALIDATE_INT  = \FILTER_VALIDATE_INT; // Validating integer value from string
+
         /**
          * @see https://www.w3schools.com/php/filter_validate_email.asp
          *
          * @param   string  $email
-         * @param           $options
+         * @param int $options
          *
          * @return  mixed
          */
-        public static function validateEmail(string $email, $options = 0)
+        public static function validateEmail(string $email, int $options = 0)
         {
-            return filter_var($email, FILTER_VALIDATE_EMAIL, $options);
+            return filter_var($email, self::VALIDATE_EMAIL, $options);
         }
     }
 
