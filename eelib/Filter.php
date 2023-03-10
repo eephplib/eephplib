b983f47ab01c17a3b34452a54e56b8888b4ca8d9
Travis van der Font <travis.font@gmail.com>
2023-03-10 16:06:02 +0100

    Added validateInteger()

--- eelib/Filter.php
+++ eelib/Filter.php
@@ -11,15 +11,15 @@ namespace eelib
 
     class Filter
     {
-        const VALIDATE_EMAI = \FILTER_VALIDATE_EMAIL; // Validating email addresses
-        const VALIDATE_IP   = \FILTER_VALIDATE_IP; // Validating IP addresses
-        const VALIDATE_INT  = \FILTER_VALIDATE_INT; // Validating integer value from string
+        const VALIDATE_EMAIL = \FILTER_VALIDATE_EMAIL; // Validating email addresses
+        const VALIDATE_IP    = \FILTER_VALIDATE_IP; // Validating IP addresses
+        const VALIDATE_INT   = \FILTER_VALIDATE_INT; // Validating integer value from string
 
         /**
          * @see https://www.w3schools.com/php/filter_validate_email.asp
          *
          * @param   string  $email
-         * @param int $options
+         * @param   int     $options
          *
          * @return  mixed
          */
@@ -27,6 +27,36 @@ namespace eelib
         {
             return filter_var($email, self::VALIDATE_EMAIL, $options);
         }
-    }
 
+        /**
+         *  Validating integer value from string
+         *
+         * @param   string  $ip_address
+         * @param   int     $options
+         *
+         * @return  mixed
+         */
+        public static function validateEmail(string $ip_address, int $options = 0)
+        {
+            return filter_var($email, self::VALIDATE_IP, $options);
+        }
+
+        /**
+         * Validating an integer value
+         *
+         * @see https://www.w3schools.com/php/filter_validate_int.asp
+         * @see https://www.php.net/manual/en/filter.filters.validate.php
+         * #see https://www.tutorialspoint.com/filter-validate-int-constant-in-php
+         * @see https://www.plus2net.com/php_tutorial/filter_validate_int.php
+         *
+         * @param   string|int|float    $integer
+         * @param   int                 $options
+         *
+         * @return  mixed
+         */
+        public static function validateInteger(string|int|float $integer, int $options = 0)
+        {
+            return filter_var($integer, self::VALIDATE_INT, $options);
+        }
+    }
 }
