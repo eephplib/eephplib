fa6c09006aa87a816fb43d445a578ba793d09e89
Travis van der Font <travis.font@gmail.com>
2023-06-14 18:03:31 +0200

    minor ArrayList refactors

--- eelib/Extended/ArrayList.php
+++ eelib/Extended/ArrayList.php
@@ -2,6 +2,11 @@
 
 namespace Extended
 {
+    use function array_map;
+    use function array_merge;
+    use function func_get_args;
+    use function is_array;
+
     /**
      * Class ArrayList Extended
      * Custom User Functions
@@ -18,44 +23,45 @@ namespace Extended
          *
          * @return  array   Returns the updated array with the new element.
          */
-        public static function push($array, $values)
+        public static function push($array, $values): array
         {
-            $args = \func_get_args();
+            $args = func_get_args();
 
             unset($args[0]);
 
-            $args = \array_merge($array, $args);
-
-            return $args;
+            return array_merge($array, $args);
         }
 
         /**
          * Push one or more elements onto the end of array with the associated key.
          * TODO: remove reference argument
+         * TODO: Needs testing
          *
          * @param   $arr
          *
          * @return  int
          */
-        public static function push_associative(&$arr)
+        public static function pushAssociative(&$arr): int
         {
-            $args = \func_get_args();
+            $args = func_get_args();
             $ret  = 0;
 
             foreach ($args as $arg)
             {
-                if (\is_array($arg))
+                if (is_array($arg))
                 {
+                    //self::pushAssociative($arg);
+
                     foreach ($arg as $key => $value)
                     {
                         $arr[$key] = $value;
                         $ret++;
                     }
+
+                    continue;
                 }
-                else
-                {
-                    $arr[$arg] = '';
-                }
+
+                $arr[$arg] = '';
             }
 
             return $ret;
