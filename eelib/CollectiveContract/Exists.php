fdb165f8e7068fe2770a8f3c129ded9ae6afdb68
Travis van der Font <travis.font@gmail.com>
2023-08-01 18:53:41 +0200

    add exists(string) to CollectiveContract

---
+++ eelib/CollectiveContract/Exists.php
@@ -0,0 +1,10 @@
+<?php namespace eelib\CollectiveContract;
+
+/**
+ * PHP Function list of methods that can be found using exits:
+ * @link https://www.php.net/manual-lookup.php?pattern=exists
+ */
+interface Exists
+{
+    public static function exists(string $filename): bool;
+}
