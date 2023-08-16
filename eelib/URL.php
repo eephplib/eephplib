87a742ecb20b0064746bc2d157505781d500e467
Travis van der Font <travis.font@gmail.com>
2023-08-16 20:12:17 +0200

    add URL

---
+++ eelib/URL.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace eelib
+{
+    /**
+     * Class URL
+     * @see     https://www.php.net/manual/en/ref.url.php
+     *
+     * @package eelib
+     * @version 2023.xx.xx
+     */
+    class URL
+    {
+        public static function base64_decode() {} // https://www.php.net/manual/en/function.base64-decode.php
+        public static function base64_encode() {} // https://www.php.net/manual/en/function.base64-encode.php
+        public static function get_headers() {} // https://www.php.net/manual/en/function.get-headers.php
+        public static function get_meta_tags() {} // https://www.php.net/manual/en/function.get-meta-tags.php
+        public static function http_build_query () {} // https://www.php.net/manual/en/function.http-build-query.php
+        public static function parse_url() {} // https://www.php.net/manual/en/function.parse-url.php
+        public static function rawurldecode() {} // https://www.php.net/manual/en/function.rawurldecode.php
+        public static function rawurlencode() {} // https://www.php.net/manual/en/function.rawurlencode.php
+        public static function urldecode() {} // https://www.php.net/manual/en/function.urldecode.php
+        public static function urlencode() {} // https://www.php.net/manual/en/function.urlencode.php
+
+    }
+}
