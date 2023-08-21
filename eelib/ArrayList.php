8fa2b6b81253813500042327481140f180ac1310
Travis van der Font <travis.font@gmail.com>
2023-08-21 22:35:00 +0200

    update to ArrayList - refactors + new functions

--- eelib/ArrayList.php
+++ eelib/ArrayList.php
@@ -1,7 +1,21 @@
 <?php
 
+declare(strict_types=1);
+
+// https://www.php.net/manual/en/class.iteratoraggregate.php#112432
 namespace eelib
 {
+
+    use function array_diff;
+    use function array_key_first;
+    use function array_key_last;
+    use function array_keys;
+    use function array_search;
+    use function array_slice;
+    use function array_values;
+    use function function_exists;
+    use function key;
+
     require_once 'Extended/ArrayList.php';
 
     /**
@@ -13,29 +27,31 @@ namespace eelib
     class ArrayList extends \Extended\ArrayList
     {
         public ArraySort $sort;
+        public ArrayIterate $iterate;
 
         public function __construct()
         {
             $this->sort = new ArraySort;
+            $this->iterate = new ArrayIterate;
         }
         /**
          * Gets the first element of an array
          *
-         * @return  mixed|null   Returns the first element of $array
+         * @return  array   Returns the first element of $array
          */
-        public static function first(array $array)
+        public static function first(array $array): array
         {
-            return \array_values($array)[0] ?? NULL;
+            return array_values($array)[0] ?? [];
         }
 
         /**
          * Gets the last element of an array
          *
-         * @return  mixed|null   Returns the last element of $array
+         * @return  array   Returns the last element of $array
          */
-        public static function last(array $array)
+        public static function last(array $array): array
         {
-            return \array_values(\array_slice($array, -1))[0] ?? NULL;
+            return array_values(array_slice($array, -1))[0] ?? [];
         }
 
         /**
@@ -48,12 +64,12 @@ namespace eelib
          */
         public static function firstKey(array $array) : ?array
         {
-            if (\function_exists('array_key_first') === TRUE)
+            if (function_exists('array_key_first') === TRUE)
             {
-                return \array_key_first($array);
+                return array_key_first($array);
             }
 
-            return $array ? \array_keys($array)[0] : NULL;
+            return $array ? array_keys($array)[0] : NULL;
         }
 
         /**
@@ -62,26 +78,57 @@ namespace eelib
          *
          * @link http://php.net/manual/en/function.array-key-last.php
          */
-        public static function lastKey(array $array) : array
+        public static function lastKey(array $array) : int|string
         {
-            if (\function_exists('array_key_first') === TRUE)
+            if (function_exists('array_key_first') === TRUE)
             {
-                return \array_key_last($array);
+                return array_key_last($array);
             }
 
-            return \key(\array_slice($array, -1, 1, TRUE));
+            return key(array_slice($array, -1, 1, TRUE));
         }
         
         // https://www.php.net/manual/en/function.array-diff.php - has no expectations
         public static function differentValues(array $array, array ...$arrays) : array
         {
-            return \array_diff($array, $arrays);
+            return array_diff($array, $arrays);
         }
         
         // https://www.php.net/manual/en/function.array-intersect.php - has no expectations
         public static function sameValues(array $array, array ...$arrays) : array
         {
-            return \array_diff($array, $arrays);
+            return array_diff($array, $arrays);
+        }
+
+        /**
+         * Searches the array for a given value and returns the first corresponding key if successful
+         *
+         * @link    https://www.php.net/manual/en/function.array-search.php
+         *
+         * @param   mixed   $needle
+         * @param   array   $haystack
+         * @param   bool    $strict
+         *
+         * @return  false|int|string
+         */
+        public static function findValue(mixed $needle, array $haystack, bool $strict = false): bool|int|string
+        {
+            return array_search($needle, $haystack, $strict);
+        }
+
+
+        /**
+         * A numeric re-indexing of all the values of an array.
+         *
+         * @link    https://www.php.net/manual/en/function.array-values
+         *
+         * @param array $array
+         *
+         * @return array    All the values from the array and indexes the array numerically.
+         */
+        public static function reindexValues(array $array): array
+        {
+            return array_values($array);
         }
     }
 }
