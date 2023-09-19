af22bd110d73855a8791c890b5e1b6adc0fcca90
Travis van der Font <travis.font@gmail.com>
2023-09-19 23:57:58 +0200

    updates to getFiles function

--- eelib/Extended/FileSystem.php
+++ eelib/Extended/FileSystem.php
@@ -4,27 +4,36 @@
 namespace Extended
 {
     /**
+     * TODO: turn methods into traits
+     *
      * Class FileSystem Extended
      * Custom User Functions
      */
     class FileSystem
     {
-        public static function getfiles($path, &$files = array())
+        public static function getFiles($path, &$files = array())
         {
-            if ( !is_dir( $path ) ) return null;
-            $handle = opendir( $path );
-
-            while ( false !== ( $file = readdir( $handle ) ) )
+            if (!is_dir($path))
             {
-                if ( $file != '.' && $file != '..' )
+                return null; // TODO: no nullpointing -> throw exception
+            }
+
+            $handle = opendir($path);
+
+            while (($file = readdir($handle)) !== false)
+            {
+                if ($file !== '.' && $file !== '..')
                 {
-                    $path2 = $path . '/' . $file;
-                    if ( is_dir( $path2 ) ) {
-                        self::{__FUNCTION__}( $path2 , $files );
-                    } else {
-                        if ( preg_match( "/\.(php|php5)$/i" , $file ) ) {
-                            $files[] = $path2;
-                        }
+                    // TODO: add proper concatenation
+                    $path2 = $path.'/'.$file;
+
+                    if (is_dir($path2))
+                    {
+                        self::{__FUNCTION__}($path2, $files);
+                    }
+                    elseif ( preg_match( "/\.(php|php5)$/i" , $file ) )
+                    {
+                        $files[] = $path2;
                     }
                 }
             }
