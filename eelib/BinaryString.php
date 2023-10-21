eb3bf119454a56403759f537d972e827d6e530b5
Travis van der Font <travis.font@gmail.com>
2023-10-21 23:56:51 +0200

    Lots of new functions (+to do) to BinaryString

--- eelib/BinaryString.php
+++ eelib/BinaryString.php
@@ -7,6 +7,14 @@ namespace eelib
 
     use eelib\Functions\BinaryString\findInitMaskLength;
 
+    use function \strspn     as strspn;
+    use function \strcspn    as strcspn;
+    use function \strcmp     as strcmp;
+    use function \strcasecmp as strcasecmp;
+    use function \strncmp    as strncmp;
+    use function \ucwords    as ucwords;
+    use function \strtok     as strtok;
+
     use RuntimeException;
 
 
@@ -16,7 +24,7 @@ namespace eelib
      * @package eelib
      * @version 2019.03.12
      */
-    class BinaryString extends \Extended\BinaryString
+    abstract class BinaryString extends \Extended\BinaryString
     {
         /**
          * Split multibyte string using regular expression pattern.
@@ -29,7 +37,7 @@ namespace eelib
          *
          * @return  string[]   The result as an array.
          */
-        public static function regular_expression_split(
+        final public static function regular_expression_split(
             string  $pattern,
             string  $string,
             int     $limit = -1
@@ -56,19 +64,19 @@ namespace eelib
  *
          * @throws RuntimeException
          */
-        public static function find_first_occurrence(
+        final public static function find_first_occurrence(
             string  $haystack,
             string  $needle,
             bool    $before_needle = false
         ) : string
         {
-            $strstr = strstr($haystack, $needle, $before_needle);
+            $stringValue = strstr($haystack, $needle, $before_needle);
 
-            if ($strstr === false) {
+            if ($stringValue === false) {
                 throw new RuntimeException('needle is not found');
             }
 
-            return $strstr;
+            return $stringValue;
         }
 
         /**
@@ -82,7 +90,7 @@ namespace eelib
          *
          * @return  string          Returns the translated string.
          */
-        public static function replace_substrings(
+        final public static function replace_substrings(
             string $string,
             string $from,
             string $to
@@ -92,12 +100,147 @@ namespace eelib
             return strtr($string, $from, $to);
         }
 
-        public static function substr() {}
-        public static function strlen() {}
-        public static function str_replace() {}
-        public static function trim() {}
-        public static function strpos() {}
-        public static function strtolower() {}
-        public static function strtoupper() {}
+        /**
+         * Finds the length of the initial segment of a string consisting entirely of characters contained within a given mask.
+         *
+         * @link http://php.net/manual/en/function.strspn.php
+         * @link http://www.w3resource.com/php/function-reference/strspn.php
+         * @link https://www.w3schools.com/php/func_string_strspn.asp
+         *
+         * @param string $subject
+         * @param string $mask
+         * @param int    $start   Default: null
+         * @param int    $length  Default: null
+         *
+         * @return int
+         */
+        final public static function FindInitMaskLength($subject, $mask, $start = NULL, $length = NULL)
+        {
+            return strspn($subject, $mask, $start, $length);
+        }
+
+        /**
+         * Find length of initial segment not matching mask
+         *
+         * @link http://php.net/manual/en/function.strcspn.php
+         * @link http://www.w3resource.com/php/function-reference/strcspn.php
+         * @link https://www.w3schools.com/php/func_string_strcspn.asp
+         *
+         * @param string $str1
+         * @param string $str2
+         * @param int    $start  Default: null
+         * @param int    $length Default: null
+         *
+         * @return int
+         */
+        final public static function FindInitNotMaskLength($str1, $str2, $start = NULL, $length = NULL)
+        {
+            return strcspn($str1, $str2, $start, $length);
+        }
+
+        /**
+         * Binary safe string comparison
+         *
+         * @link http://php.net/manual/en/function.strcmp.php
+         *
+         * @param string $str1
+         * @param string $str2
+         *
+         * @return int
+         */
+        final public static function Compare($str1, $str2)
+        {
+            return strcmp($str1, $str2);
+        }
+
+        /**
+         * Binary safe case-insensitive string comparison
+         *
+         * @link http://php.net/manual/en/function.strcasecmp.php
+         *
+         * @param string $str1
+         * @param string $str2
+         *
+         * @return int
+         */
+        final public static function CompareCaseInsensitive($str1, $str2)
+        {
+            return strcasecmp($str1, $str2);
+        }
+
+        /**
+         * Binary safe string comparison of the first n characters
+         *
+         * @link http://php.net/manual/en/function.strncmp.php
+         *
+         * @param string $str1
+         * @param string $str2
+         * @param int    $len
+         *
+         * @return int
+         */
+        final public static function CompareFirstCharacters($str1, $str2, $len)
+        {
+            return strncmp($str1, $str2, $len);
+        }
+
+        /**
+         * Uppercase the first character of each word in a string
+         *
+         * @link http://php.net/manual/en/function.ucwords.php
+         *
+         * @param $string
+         * @param string $delimiters
+         *
+         * @return string
+         */
+        final public static function UpperCaseWords($string, $delimiters = ' \t\r\n\f\v')
+        {
+            return ucwords($string, $delimiters);
+        }
+
+        /**
+         * @param   $string
+         * @param   $token
+         * @return  false|string
+         */
+        final public static function SubstringExtract($string, $token)
+        {
+            return strtok($string, $token);
+        }
+
+        /**
+         * Checks if all of the characters in the provided string text are hexadecimal 'digits'.
+         *
+         * @link http://php.net/manual/en/function.ctype-xdigit.php
+         *
+         * @param $text
+         *
+         * @return bool
+         */
+        final public static function isHexadecimalDigit($text)
+        {
+            return ctype_xdigit($text);
+        }
+
+        final public static function substr() {}
+        final public static function strlen() {}
+        final public static function str_replace() {}
+        final public static function trim() {}
+        final public static function strpos() {}
+        final public static function strtolower() {}
+        final public static function strtoupper() {}
     }
 }
+
+// TODO:
+// ctype_alnum
+// ctype_alpha
+// ctype_cntrl
+// ctype_digit
+// ctype_graph
+// ctype_lower
+// ctype_print
+// ctype_punct
+// ctype_space
+// ctype_upper
