c2a771c0ab78c052c13037357698decab374246f
Travis van der Font <travis.font@gmail.com>
2024-03-24 23:14:55 +0100

    Minor update to SearchStringAllocator (WIP + needs testing)

--- eelib/Extended/BinaryString/SearchStringAllocator.php
+++ eelib/Extended/BinaryString/SearchStringAllocator.php
@@ -8,28 +8,42 @@ namespace eelib\Functions\BinaryString\Extended
     use function \strlen;
     use function \strtolower;
     use function \trim;
+
     // TODO: This needs to be tested.
     trait SearchStringAllocator
     {
+
+        private static int $safe_length = 0;
 
         final public static function searchStringAllocator(
             string  $search_string,
             int     $safe_length = 3
         ) : array
         {
+            // $safe_length cannot be below 0 throwargument
+
             $search_string = strtolower(trim($search_string));
+            $keywords      = []; // is this a list or array?
 
             if (strlen($search_string) >= $safe_length)
             {
-                $removals = [
+                self::$safe_length = $safe_length;
+                $removals          = [
                     '~', '~', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '=',
                     ',', '.', '?', '/', ':', ";", '"', '\'', '[', '{', ']', '}', '|', '\\'
                 ];
 
                 $search_string   = str_replace($removals, ' ', $search_string);
                 $search_keywords = explode(' ', $search_string);
+                $keywords        = array_filter( // perhapos this should be an function entirely?
+                                    array:    $search_keywords,
+                                    callback: [__CLASS__, '_callbackKeywords']
+                                   );
+
+                /*
                 $keywords        = array_filter(array:    $search_keywords,
-                                                callback: static function ($data) use ($safe_length)
+                                                callback: static function ($data)
+                                                          use             ($safe_length)
                 {
                     if (strlen($data) >= $safe_length)
                     {
@@ -38,8 +52,20 @@ namespace eelib\Functions\BinaryString\Extended
                     // TODO: what is teh default return (avoid null)?
                 });
 
-                return array_values($keywords);
+                */
             }
+
+            return array_values($keywords);
+
+        }
+
+        private static function _callbackKeywords(string $data)
+        {
+            if (strlen($data) >= self::$safe_length)
+            {
+                return $data;
+            }
+            // TODO: what is teh default return (avoid null)?
         }
 
 // ?q=this%20is%20my%20name.is%20cool%20-%20name-caller
