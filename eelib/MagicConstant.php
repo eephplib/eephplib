ee26db60cba173411258618b5f8d90ec31b880fc
Travis van der Font <travis.font@gmail.com>
2025-06-11 23:57:03 +0200

    wip MagicConstant

---
+++ eelib/MagicConstant.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace eelib
+{
+    /**
+     * Class MagicConstant
+     *
+     * @package eelib
+     * @version 2025.xx.xx
+     */
+    class MagicConstant
+    {
+        // __FILE__
+        // __LINE__
+        // __DIR__
+        // NOTE: __DIR__ is the recommended and cleaner approach over dirname(__FILE__) in modern PHP (PHP 5.3 and later).
+        // It’s more concise and readable. It's a language construct, so it’s marginally faster (though the difference is negligible in practice).
+    }
+}
